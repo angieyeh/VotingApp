07de5c527fdd09c8252320e194a225975c7edaec
angie88 <angieyeh24@gmail.com>
2016-05-28 18:16:25 -0700

    wrote tests for reducer function - testing to make sure we set our entries up for voting

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -5,15 +5,27 @@ import reducer from '../src/reducer';
 
 describe('reducer', () => {
   it('setting entries', () => {
-    const initialState = fromJS({
-      entries: []
-    });
+    const initialState = Map();
     let setEntriesAction = {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']};
     const nextState = reducer(initialState, setEntriesAction);
     expect(nextState).to.equal(fromJS({
       entries: ['Trainspotting', '28 Days Later']
     }))
   });
+
+  it('sets movies for voting', () => {
+    const initialState = fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    let nextAction = {type: 'VOTE'};
+    const nextState = reducer(initialState, nextAction)
+    expect(nextState).to.equal(fromJs({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }))
+  })
 
   it('handles vote', () => {
     const initialState = fromJS({
